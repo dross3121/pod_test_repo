570d70f3fd8a0c94d62c72fd3d8371e95cc9ceac
Kayla Ousley <kaousley22@gmail.com>
2021-09-16 20:36:09 -0400

    primitive datatypes challenge

---
+++ kayla/temperature.py
@@ -0,0 +1,21 @@
+celsius_100 = (100 - 32) * 5/9
+print(celsius_100)
+print(type(celsius_100))
+
+celsius_0 = (0-32) * 5/9
+print(celsius_0)
+
+print((30 * 1.8) + 32)
+print((34.2 * 9/5) + 32)
+
+# 30 degrees celsius is hotter by one degree
+print((5 - 32) * 5 / 9)
+far = (30.2 * 1.8) + 32
+print(far)
+if (far > 85.1):
+    print(far, "is bigger by", (far - 85.1))
+    # else:
+    # print(far, "is smaller by" (85.1 - far))
+    print(far)
+else:
+    print(85.1, "is bigger")

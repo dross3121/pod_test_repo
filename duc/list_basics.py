89986807a70e69814b301dccc1eafcb095ea8df0
DTA <duchongta0703@gmail.com>
2021-09-20 15:34:28 -0700

    Updated LIST_BASICS.PY

---
+++ duc/list_basics.py
@@ -0,0 +1,26 @@
+# HOW TO CREATE A LIST
+grocery_list = ["apples", "pasta", "milk"]
+print(grocery_list)
+
+# You can make list from Booleans, Integers, and Floats as well as Strings.
+
+integer_list = [-1, 0, 1]
+print(integer_list)
+float_list = [1.74, 99.99, -0.23]
+print(float_list)
+boolean_list = [True, False]
+print(boolean_list)
+
+mixed_list = [1.0, -1, False, "Hello World"]
+print(mixed_list)
+
+item_one = "apples"
+item_two = "pasta"
+item_three = "milk"
+
+grocery_list_two = [item_one, item_two, item_three]
+print(grocery_list_two)
+
+empty_list = []
+print(empty_list)
+print(type(empty_list))

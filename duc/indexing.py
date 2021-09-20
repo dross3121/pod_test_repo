94a58610c7b70003e69af28bf3ce887fa214b63a
DTA <duchongta0703@gmail.com>
2021-09-20 15:33:51 -0700

    Updated INDEXING.PY

---
+++ duc/indexing.py
@@ -0,0 +1,16 @@
+# INDEXING~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+musicians = ["Drake", "Beyonce", "The Weeknd", "Eminem"]
+            #   0          1           2           3
+
+            #   -4        -3          -2          -1
+
+-4, -3, -2, -1, 0, 1, 2, 3
+
+# print(musicians[-4])
+
+# musicians[-4] = "Kanye West"
+
+# print(musicians[-4])
+# print(musicians)
+
+print(musicians[-4])

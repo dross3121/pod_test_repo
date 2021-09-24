b187aaa54696d59d408e6df06d6a0a01293c7742
Kayla Ousley <kaousley22@gmail.com>
2021-09-23 20:18:54 -0400

    restaraunts challenge completed

--- kayla/box_office_challenge.py
+++ kayla/box_office_challenge.py
@@ -2,8 +2,7 @@
 # this variable is assigned a string
 movies_str = "House on Haunted Hill,Cruel Intentions,10 Things I Hate About You,My Favorite Martian,8MM,Fight Club,The Matrix,Any Given Sunday,The Thin Red Line,A Bug's Life, For Love of the Game, Instinct, Mickey Blue Eyes, The Best Man, Bicentennial Man, The 13th Warrior, October Sky, Lake Placid, Random Hearts, Mighty Joe Young, Superstar, Mystery Men, The Talented Mr. Ripley, Dogma, The Out-of-Towners, The Other Sister, Baby Geniuses, The Story of Us, Blast from the Past, The Insider, Saving Private Ryan, Mysteries of Egypt, The Wood, Arlington Road, T-Rex: Back to the Cretaceous, The Iron Giant, Edtv, At First Sight, The Faculty, Summer of Sam, The Bachelor, Stir of Echoes, Anna and the King, Man on the Moon, Galaxy Quest, Enemy of the State, Waking Ned Devine, Doug's 1st Movie,An Ideal Husband,Everest"
 # this variable is assigned a list of strings
-top_50_list = ['Star Wars: Episode I - The Phantom Menace', 'The Sixth Sense', 'Austin Powers: The Spy Who Shagged Me', 'Toy Story 2', 'The Matrix', 'Tarzan', 'Big Daddy', 'The Mummy', 'Runaway Bride', 'The Blair Witch Project', 'Notting Hill', 'The World Is Not Enough', 'Double Jeopardy', 'Wild Wild West', 'Analyze This', "The General's Daughter", 'American Pie', 'Inspector Gadget', 'Shakespeare in Love', 'Sleepy Hollow', 'The Haunting', 'Patch Adams', 'Entrapment', 'Pokémon: The First Movie - Mewtwo Strikes Back', 'Payback', 'Deep Blue Sea', 'American Beauty', 'The Thomas Crown Affair', 'Stuart Little', 'Blue Streak', 'The Green Mile', 'Bowfinger', 'Life', 'The Bone Collector', "She's All That", 'End of Days', 'Three Kings', 'A Civil Action', 'Stepmom', 'Eyes Wide Shut', 'Never Been Kissed', 'Forces of Nature', 'Varsity Blues', 'Message in a Bottle', "You've Got Mail", 'South Park: Bigger, Longer & Uncut', 'Stigmata', 'Life Is Beautiful', 'The Prince of Egypt',
-               'Deuce Bigalow: Male Gigolo']
+top_50_list = ['Star Wars: Episode I - The Phantom Menace', 'The Sixth Sense', 'Austin Powers: The Spy Who Shagged Me', 'Toy Story 2', 'The Matrix', 'Tarzan', 'Big Daddy', 'The Mummy', 'Runaway Bride', 'The Blair Witch Project', 'Notting Hill', 'The World Is Not Enough', 'Double Jeopardy', 'Wild Wild West', 'Analyze This', "The General's Daughter", 'American Pie', 'Inspector Gadget', 'Shakespeare in Love', 'Sleepy Hollow', 'The Haunting', 'Patch Adams', 'Entrapment', 'Pokémon: The First Movie - Mewtwo Strikes Back', 'Payback', 'Deep Blue Sea', 'American Beauty', 'The Thomas Crown Affair', 'Stuart Little', 'Blue Streak', 'The Green Mile', 'Bowfinger', 'Life', 'The Bone Collector', "She's All That", 'End of Days', 'Three Kings', 'A Civil Action', 'Stepmom', 'Eyes Wide Shut', 'Never Been Kissed', 'Forces of Nature', 'Varsity Blues', 'Message in a Bottle', "You've Got Mail", 'South Park: Bigger, Longer & Uncut', 'Stigmata', 'Life Is Beautiful', 'The Prince of Egypt','Deuce Bigalow: Male Gigolo']
 
 
 # TODO: Take the string "movies_str" and convert it into a list and assign it to a variable called "remaining_50_list"
@@ -21,7 +20,7 @@ print()
 
 # TODO: We're down to 49 movies in "remaining_50_list". Let's add a movie to the list called "Anywhere But Here"
 # Note: Print the variable "remaining_50_list" and see if the movie has been added!
-remaining_50_list.append("Spidey Senses")
+remaining_50_list.append("Anywhere but here")
 print(remaining_50_list)
 print()
 print()
@@ -50,5 +49,5 @@ print()
 print()
 # TODO: Let's convert the list "top_100" back into a string. Assign it to a variable called "top_100_str".
 # Note: Print your new variable to see the string version of the movies
-top_100_str = ''.join([str(item) for item in top_100])
+top_100_str = ' *** '.join([str(item) for item in top_100])
 print(top_100_str)

f9f6d8381e095eebc615e68770c9aba42649c97a
Kayla Ousley <kayousley22@gmail.com>
2021-10-17 08:58:59 -0400

    Stations challenge complete

---
+++ kayla/book_class.py
@@ -0,0 +1,21 @@
+class Booklist():
+  
+	def __init__(self):
+		self.books = []
+   
+	def add(self, title, author):
+	  book = {},
+      self["title"] = title
+      self["author"] = author
+    
+      self.books.append(book)
+
+	def count_books(self):
+		pass
+
+	def remove_title(self, title):
+		pass
+
+	def display_titles(self):
+		pass
+

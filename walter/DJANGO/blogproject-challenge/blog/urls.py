1a6a24be4c92ea69070fcd449063770130f1dd67
Lavoing <lavoing@gmail.com>
2021-11-03 22:58:56 -0400

    Don't know

---
+++ walter/DJANGO/blogproject-challenge/blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # blog/
+    path('', views.blog, name='blog'),
+    # blog/edit/<int:post_id>
+    path('edit/<int:post_id>', views.edit, name='edit')
+]

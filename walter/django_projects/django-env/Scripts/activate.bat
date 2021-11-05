9e79bf5cf708be79e54d95c333528cc9dc359373
Walter <lavoing@gmail.com>
2021-11-05 12:05:44 -0400

    Modified files

--- walter/django_projects/django-env/Scripts/activate.bat
+++ walter/django_projects/django-env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Walter Dunn\Desktop\GitHub\pod_test_repo\walter\DJANGO\django-env
+set VIRTUAL_ENV=C:\Users\Walter Dunn\Desktop\GitHub\pod_test_repo\walter\django_projects\django-env
 
 if not defined PROMPT set PROMPT=$P$G
 
